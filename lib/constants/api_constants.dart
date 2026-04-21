9ae4d6da83724ade1183d23a39be8f61075c63f0
Ayeshath Azmiya shiri <162473054+azmiyashiri@users.noreply.github.com>
2026-04-21 16:48:18 +0530

    new update

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
 
   // static const String baseUrl = "https://rajasree.astradevelops.in/CareerApp/public/api/";
-  static const String baseUrl = "http://careerss.in/api/";
+  static const String baseUrl = "https://careerss.in/api/";
 
   static const String login = "login";
   static const String signup = "signup";
